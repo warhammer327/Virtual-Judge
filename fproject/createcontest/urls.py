5f16fa5a3cf6c6978706a5aa31e170d6b2c5f706
warhammer327 <rizwansm@outlook.com>
2020-06-10 09:24:00 +0600

    each problem of diff contets are now accessible

--- fproject/createcontest/urls.py
+++ fproject/createcontest/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
 	#url(r'^$',views.firstpage,name='firstpage'),
 	#url(r'^about/$',views.about,name='about'),
 	url(r'^home/(?P<pk>\d+)/$',views.problemlist,name='problemlist'),
-	url(r'^home/(?P<pk>\d+)/problem/$',views.problem,name='problem'),
+	url(r'^home/(?P<pk>\d+)/one/$',views.one,name='one'),
+	url(r'^home/(?P<pk>\d+)/two/$',views.two,name='two'),
+	url(r'^home/(?P<pk>\d+)/three/$',views.three,name='three'),
+	url(r'^home/(?P<pk>\d+)/four/$',views.four,name='four'),
+	url(r'^home/(?P<pk>\d+)/five/$',views.five,name='five'),
 	#url(r'^home/(?P<pk>\d+)/(?P<id>\d+)/$',views.problemdetail,name='problemdetail'),
 	#url(r'^home/$',views.home,name='home'),
 
--- fproject/createcontest/views.py
+++ fproject/createcontest/views.py
@@ -8,7 +8,32 @@ def problemlist(request,pk):
 	args = {'sitename':sitename,'contest':contest}
 	return render(request,'front/problemlist.html',args)
 
-def problem(request,pk):
-	sitename = 'Problem'
+def one(request,pk):
+	sitename = 'Problem 1'
 	contest = Createcontest.objects.filter(pk=pk)
-	return render(request,'front/problem.html')
+	args = {'sitename':sitename,'contest':contest}
+	return render(request,'front/one.html',args)
+
+def two(request,pk):
+	sitename = 'Problem 2'
+	contest = Createcontest.objects.filter(pk=pk)
+	args = {'sitename':sitename,'contest':contest}
+	return render(request,'front/two.html',args)
+
+def three(request,pk):
+	sitename = 'Problem 3'
+	contest = Createcontest.objects.filter(pk=pk)
+	args = {'sitename':sitename,'contest':contest}
+	return render(request,'front/three.html',args)
+
+def four(request,pk):
+	sitename = 'Problem 4'
+	contest = Createcontest.objects.filter(pk=pk)
+	args = {'sitename':sitename,'contest':contest}
+	return render(request,'front/four.html',args)
+
+def five(request,pk):
+	sitename = 'Problem 5'
+	contest = Createcontest.objects.filter(pk=pk)
+	args = {'sitename':sitename,'contest':contest}
+	return render(request,'front/five.html',args)
